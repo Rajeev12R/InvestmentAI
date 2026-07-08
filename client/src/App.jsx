05981fc9c9945465c98cd6f5594b9a4c85e8fe0c
Rajeev Ranjan <rjranjan2112@gmail.com>
2026-07-08 16:01:06 +0530

    Main page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import LandingPage from './components/LandingPage';
+import DashboardPage from './components/DashboardPage';
 
 const App = () => {
   return (
-    <div className='text-red-500'>App</div>
-  )
-}
+    <Router>
+      <div className="min-h-screen text-slate-100 flex flex-col font-sans antialiased selection:bg-emerald-500/20 selection:text-emerald-300">
+        <Navbar />
+        <main className="flex-1 flex flex-col">
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/company/:ticker" element={<DashboardPage />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  );
+};
 
-export default App
+export default App;
